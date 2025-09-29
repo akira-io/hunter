a8213a009d6d4a642632cd6ed9d36b7e5e23caa7
kid <kidiatoliny@akira-io.com>
2025-09-29 15:34:44 +0200

    feat: enhance UI with smooth animations, improved button interactions, and refined card effects

--- resources/js/components/ui/card.tsx
+++ resources/js/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<'div'>) {
         <div
             data-slot='card'
             className={cn(
-                'gradient effect text-card-foreground flex flex-col gap-6 rounded-2xl  py-6 shadow-md bg-card',
+                'gradient effect text-card-foreground flex flex-col gap-6 rounded-2xl py-6 shadow-md bg-card',
                 className
             )}
             {...props}
