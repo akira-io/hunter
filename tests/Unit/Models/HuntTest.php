722e4a38ccd751245f617f031ed26b097f14cefb
kid <kidiatoliny@akira-io.com>
2025-10-03 17:33:02 +0200

    test: update assertion in HuntTest for canonicalized key comparison

--- tests/Unit/Models/HuntTest.php
+++ tests/Unit/Models/HuntTest.php
@@ -9,7 +9,7 @@ test('to array', function () {
     $hunt = Hunt::factory()->create()->refresh();
 
     expect(array_keys($hunt->toArray()))
-        ->toBe([
+        ->toEqualCanonicalizing([
             'id',
             'owner_id',
             'content',
